8035f0a017531354e73ce9b291289e4d835bfc33
Yan Wang <Yan@Flatirons-Air-8.fios-router.home>
2018-09-05 20:41:06 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,14 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories
 
+  def categories_attributes=(categories_attributes)
+    categories_attributes.each do |i, category_attributes|
+      if category_attributes[:name] != ""
+        self.categories << Category.find_or_create_by(category_attributes)
+      end
+    end
+  end
 
 end
